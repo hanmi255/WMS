ef296e5c5de993462e691426d187b7475c6d1e05
ForeverOMC <ForeverOMC@163.com>
2025-02-22 17:30:56 +0800

    合并噗叽分支_0_0

---
+++ wms-cpp/wms-c3-chuweiguanli/controller/cangkudingyi/StoreController.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"	
+#include "controller/cangkudingyi/StoreController.h"
+
+StringJsonVO::Wrapper StoreController::execAddStore(const StoreAddDTO::Wrapper dto) {
+	return StringJsonVO::Wrapper();
+}
+
+StringJsonVO::Wrapper StoreController::execModifyStore(const StoreModifyDTO::Wrapper dto)
+{
+	return StringJsonVO::Wrapper();
+}
