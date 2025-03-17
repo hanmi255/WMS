97f78221e1ea62786135e903d4013270fa0c2e51
ForeverOMC <ForeverOMC@163.com>
2025-03-17 19:52:25 +0800

    合并分支sheng

--- wms-cpp/wms-c3-chuweiguanli/controller/Router.cpp
+++ wms-cpp/wms-c3-chuweiguanli/controller/Router.cpp
@@ -38,7 +38,6 @@
 #include "controller/chuweidingyi/ChuWeiController.h"
 #include "controller/chuweidingyi/ChuWeiDingYiController.h"
 #include"kongchuwei/GetKongChuWeiListMycontroller.h"
-#include "controller/chuweidingyi/chuweixaingxixinxidaorudaochuCONTROLLER.h"
 
 // 如果定义了关闭Swagger文档宏
 #ifdef CLOSE_SWAGGER_DOC
@@ -74,7 +73,6 @@ void Router::initRouter()
 	ROUTER_SIMPLE_BIND(BinController);
 	ROUTER_SIMPLE_BIND(ChuWeiController);
 	ROUTER_SIMPLE_BIND(GetKongChuWeiListMycontroller);
-	ROUTER_SIMPLE_BIND(apixiangxixinxi);
 }
 	
 #ifdef HTTP_SERVER_DEMO
